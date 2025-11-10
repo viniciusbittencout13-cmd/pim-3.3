fdcbfa0905a2892997dcad5bbe38dbce12d8b70c
viniciusbittencout13-cmd <viniciusbittencout.13@gmail.com>
2025-11-10 12:00:53 -0300

    Create ConfiguracaoPage.xaml.cs

---
+++ GLLRV.DesktopApp/Views/Pages/ConfiguracaoPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace GLLRV.DesktopApp.Views.Pages
+{
+    public partial class ConfiguracaoPage : Page
+    {
+        public ConfiguracaoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

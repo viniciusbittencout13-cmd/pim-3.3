892496e9dd6a819dd1faf8d9cf9d0ea717392eab
viniciusbittencout13-cmd <viniciusbittencout.13@gmail.com>
2025-11-10 11:59:47 -0300

    Create RelatoriosPage.xaml.cs

---
+++ GLLRV.DesktopApp/Views/Pages/RelatoriosPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace GLLRV.DesktopApp.Views.Pages
+{
+    public partial class RelatoriosPage : Page
+    {
+        public RelatoriosPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

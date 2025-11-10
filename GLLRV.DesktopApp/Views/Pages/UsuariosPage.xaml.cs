382a17f7fc7bd417000900164b4b4d6116ffd627
viniciusbittencout13-cmd <viniciusbittencout.13@gmail.com>
2025-11-10 15:58:45 -0300

    Update UsuariosPage.xaml.cs

--- GLLRV.DesktopApp/Views/Pages/UsuariosPage.xaml.cs
+++ GLLRV.DesktopApp/Views/Pages/UsuariosPage.xaml.cs
@@ -2,7 +2,7 @@ using System.Windows.Controls;
 
 namespace GLLRV.DesktopApp.Views.Pages
 {
-    public partial class UsuariosPage : Page
+    public partial class UsuariosPage : UserControl
     {
         public UsuariosPage()
         {

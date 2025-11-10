613d1115d580b49a69e394ddf3749c03fc074232
viniciusbittencout13-cmd <viniciusbittencout.13@gmail.com>
2025-11-10 15:58:05 -0300

    Update HistoricoChamadosPage.xaml.cs

--- GLLRV.DesktopApp/Views/Pages/HistoricoChamadosPage.xaml.cs
+++ GLLRV.DesktopApp/Views/Pages/HistoricoChamadosPage.xaml.cs
@@ -3,7 +3,7 @@ using GLLRV.DesktopApp.Services;
 
 namespace GLLRV.DesktopApp.Views.Pages
 {
-    public partial class HistoricoChamadosPage : Page
+    public partial class HistoricoChamadosPage : UserControl
     {
         public HistoricoChamadosPage()
         {

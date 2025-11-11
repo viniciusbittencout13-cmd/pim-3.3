946fb9f2cc6725171ef3759b3c084e97780ff2e1
viniciusbittencout13-cmd <viniciusbittencout.13@gmail.com>
2025-11-11 15:12:30 -0300

    Refactor example data for ChamadosPendentesPage
    Updated the data structure for example items in the ChamadosDataGrid.

--- GLLRV.DesktopApp/Views/Pages/ChamadosPendentesPage.xaml.cs
+++ GLLRV.DesktopApp/Views/Pages/ChamadosPendentesPage.xaml.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Windows.Controls;
 
 namespace GLLRV.DesktopApp.Views.Pages
@@ -8,12 +9,14 @@ namespace GLLRV.DesktopApp.Views.Pages
         {
             InitializeComponent();
 
-            // Só pra preencher com dados fake por enquanto
-            ChamadosDataGrid.ItemsSource = new[]
+            // Dados de exemplo / depois você troca pelo serviço real
+            var chamados = new List<dynamic>
             {
-                new { Usuario = "Gustavo", NivelPrioridade = "Alto", Dificuldade = "Média", DataAbertura = "11/11/2025", Horario = "15:30" },
-                new { Usuario = "Vinícius", NivelPrioridade = "Médio", Dificuldade = "Baixa", DataAbertura = "11/11/2025", Horario = "16:00" }
+                new { Usuario = "Gustavo",  NivelPrioridade = "Alto",  Dificuldade = "Média", Data = "11/11/2025", Horario = "15:30" },
+                new { Usuario = "Vinícius", NivelPrioridade = "Médio", Dificuldade = "Baixa", Data = "11/11/2025", Horario = "16:00" }
             };
+
+            ChamadosDataGrid.ItemsSource = chamados;
         }
     }
 }

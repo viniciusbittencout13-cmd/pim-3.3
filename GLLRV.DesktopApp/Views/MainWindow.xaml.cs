e093f64ecfe0671a75585d16008d0184ac473412
viniciusbittencout13-cmd <viniciusbittencout.13@gmail.com>
2025-11-10 23:54:28 -0300

    Add method to open pending calls page

--- GLLRV.DesktopApp/Views/MainWindow.xaml.cs
+++ GLLRV.DesktopApp/Views/MainWindow.xaml.cs
@@ -113,5 +113,15 @@ namespace GLLRV.DesktopApp.Views
         {
             Application.Current.Shutdown();
         }
+        private void AbrirChamadosPendentes()
+{
+    ContentArea.Children.Clear();
+    ContentArea.Children.Add(new GLLRV.DesktopApp.Views.Pages.ChamadosPendentesPage());
+}
+
+private void ChamadosPendentesButton_Click(object sender, RoutedEventArgs e)
+{
+    AbrirChamadosPendentes();
+}
     }
 }

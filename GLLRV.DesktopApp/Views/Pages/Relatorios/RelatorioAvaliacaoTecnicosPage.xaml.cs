ad177b1370f872777f9297333541d54fb183df01
viniciusbittencout13-cmd <viniciusbittencout.13@gmail.com>
2025-11-13 09:33:04 -0300

    Update RelatorioAvaliacaoTecnicosPage.xaml.cs

--- GLLRV.DesktopApp/Views/Pages/Relatorios/RelatorioAvaliacaoTecnicosPage.xaml.cs
+++ GLLRV.DesktopApp/Views/Pages/Relatorios/RelatorioAvaliacaoTecnicosPage.xaml.cs
@@ -1,1 +1,38 @@
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using GLLRV.DesktopApp.Services;
 
+namespace GLLRV.DesktopApp.Views.Pages.Relatorios
+{
+    public partial class RelatorioAvaliacaoTecnicosPage : UserControl
+    {
+        public RelatorioAvaliacaoTecnicosPage()
+        {
+            InitializeComponent();
+            PeriodoCombo.ItemsSource = new[] { "Todos", "Madrugada", "Manhã", "Tarde", "Noite" };
+            CategoriaCombo.ItemsSource = new[] { "Todos", "Infraestrutura", "Sistemas", "Rede", "Hardware", "Software", "Outros" };
+            PrioridadeCombo.ItemsSource = new[] { "Todas", "Baixa", "Média", "Alta", "Crítica" };
+            TecnicoCombo.ItemsSource = JsonDataStore.LoadChamados()
+                .Select(c => c.Responsavel).Where(s => !string.IsNullOrWhiteSpace(s)).Distinct()
+                .Prepend("Todos").ToList();
+
+            PeriodoCombo.SelectedIndex = CategoriaCombo.SelectedIndex =
+                PrioridadeCombo.SelectedIndex = TecnicoCombo.SelectedIndex = 0;
+        }
+
+        private static T? FindParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            var p = VisualTreeHelper.GetParent(child);
+            while (p != null && p is not T) p = VisualTreeHelper.GetParent(p);
+            return p as T;
+        }
+
+        private void Voltar_Click(object sender, RoutedEventArgs e)
+        {
+            var frame = FindParent<Frame>(this);
+            if (frame != null) frame.Content = new RelatoriosPage();
+        }
+    }
+}

796e5a3b28edadd3e97b71877879d98652479303
viniciusbittencout13-cmd <viniciusbittencout.13@gmail.com>
2025-11-13 08:46:04 -0300

    Update RelatoriosPage.xaml.cs

--- GLLRV.DesktopApp/Views/Pages/RelatoriosPage.xaml.cs
+++ GLLRV.DesktopApp/Views/Pages/RelatoriosPage.xaml.cs
@@ -1,3 +1,4 @@
+using System.Windows;
 using System.Windows.Controls;
 
 namespace GLLRV.DesktopApp.Views.Pages
@@ -7,6 +8,25 @@ namespace GLLRV.DesktopApp.Views.Pages
         public RelatoriosPage()
         {
             InitializeComponent();
+        }
+
+        private void Tempo_Click(object sender, RoutedEventArgs e) =>
+            Navigate(new RelatorioGenericoPage("TEMPO DE ATENDIMENTO"));
+
+        private void AvaliacaoAtend_Click(object sender, RoutedEventArgs e) =>
+            Navigate(new RelatorioGenericoPage("AVALIAÇÃO DOS ATENDIMENTOS"));
+
+        private void AvaliacaoTec_Click(object sender, RoutedEventArgs e) =>
+            Navigate(new RelatorioGenericoPage("AVALIAÇÃO DOS TÉCNICOS"));
+
+        private void Atendimentos_Click(object sender, RoutedEventArgs e) =>
+            Navigate(new RelatorioGenericoPage("ATENDIMENTOS"));
+
+        // Usa o método do MainWindow para trocar o conteúdo do frame
+        private void Navigate(UserControl page)
+        {
+            var mw = Application.Current.MainWindow as MainWindow;
+            mw?.Navigate(page);
         }
     }
 }

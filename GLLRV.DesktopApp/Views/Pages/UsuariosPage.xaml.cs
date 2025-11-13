c7282430807e9d0585f82bc81b1128f6a442dfa6
viniciusbittencout13-cmd <viniciusbittencout.13@gmail.com>
2025-11-13 13:38:06 -0300

    Update UsuariosPage.xaml.cs

--- GLLRV.DesktopApp/Views/Pages/UsuariosPage.xaml.cs
+++ GLLRV.DesktopApp/Views/Pages/UsuariosPage.xaml.cs
@@ -1,4 +1,6 @@
+using System.Windows;
 using System.Windows.Controls;
+using GLLRV.DesktopApp.Views.Pages.Usuarios;
 
 namespace GLLRV.DesktopApp.Views.Pages
 {
@@ -7,6 +9,46 @@ namespace GLLRV.DesktopApp.Views.Pages
         public UsuariosPage()
         {
             InitializeComponent();
+        }
+
+        private void CadastroClienteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Window.GetWindow(this) is MainWindow main)
+            {
+                main.MainContentFrame.Navigate(new CadastroClientePage());
+            }
+        }
+
+        private void CadastroTecnicoButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Window.GetWindow(this) is MainWindow main)
+            {
+                main.MainContentFrame.Navigate(new CadastroTecnicoPage());
+            }
+        }
+
+        private void EditarClienteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Window.GetWindow(this) is MainWindow main)
+            {
+                main.MainContentFrame.Navigate(new EditarClientePage());
+            }
+        }
+
+        private void EditarTecnicoButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Window.GetWindow(this) is MainWindow main)
+            {
+                main.MainContentFrame.Navigate(new EditarTecnicoPage());
+            }
+        }
+
+        private void VoltarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Window.GetWindow(this) is MainWindow main)
+            {
+                main.MainContentFrame.Navigate(new ChamadosPendentesPage());
+            }
         }
     }
 }

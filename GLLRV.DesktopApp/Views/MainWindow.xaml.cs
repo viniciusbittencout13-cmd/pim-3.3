3e8d4ed2176b695568c88b276c06599b3e9e82ac
viniciusbittencout13-cmd <viniciusbittencout.13@gmail.com>
2025-11-14 18:03:41 -0300

    Update MainWindow.xaml.cs

--- GLLRV.DesktopApp/Views/MainWindow.xaml.cs
+++ GLLRV.DesktopApp/Views/MainWindow.xaml.cs
@@ -70,7 +70,7 @@ namespace GLLRV.DesktopApp.Views
 
         // Faltava esse handler no seu build
         private void ConfiguracaoButton_Click(object sender, RoutedEventArgs e)
-            => MainContentFrame.Navigate(new ConfiguracoesPage(_usuario));
+            => MainContentFrame.Navigate(new ConfiguracaoPage(_usuario));
             
         private void SairButton_Click(object sender, RoutedEventArgs e)
             => Close();
